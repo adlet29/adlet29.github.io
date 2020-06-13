4457148d45a61966c4564544a0d9f4ace50cbfb4
adlet29 <adletassanov26@gmail.com>
2020-06-13 23:40:31 +0600

    qqq

--- backend/get.php
+++ backend/get.php
@@ -5,19 +5,18 @@ require 'DB.php';
 function li(){
     $dbConn = DB::get();
     
-    $result = pg_query_params($dbConn, 'SELECT name FROM pg_prepared_statements WHERE name = $1', array("GET345"));
+    // $result = pg_query_params($dbConn, 'SELECT name FROM pg_prepared_statements WHERE name = $1', array("GET345"));
 
-    if (pg_num_rows($result) == 0) {
+    // if (pg_num_rows($result) == 0) {
         // $result = pg_prepare($dbConn, "GET2", 'SELECT * FROM pg_stat_activity WHERE datname =  $1');
-        pg_prepare($dbConn, "GET35", 'SELECT * FROM public.machinery_bu');
-    }
+        pg_prepare($dbConn, "GET34", 'SELECT * FROM public.machinery_bu');
+    // }
 
     
     $result = [];
-    $res = pg_execute($dbConn, "GET35",[]);
+    $res = pg_execute($dbConn, "GET34",[]);
     if ($res === false) {
-        // header('Location: /login.html');
-        // print_r(pg_result_error($dbConn));
+        print_r(pg_result_error($dbConn));
     } else {
         $cnt = 0;
         // $arr = pg_fetch_array($res, NULL, PGSQL_NUM);
